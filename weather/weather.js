f803f3f498480b4981c76105abecf36620a79654
bjoh213 <77363316+bjoh213@users.noreply.github.com>
2021-04-09 23:17:40 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -2,7 +2,7 @@
 var map = L.map('map').setView([38, -95], 4);
 
 // Add OpenStreetMap to the map
-var basemapUrl = 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}.png';
+var basemapUrl = 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
 
 // Add weather radar to the map
